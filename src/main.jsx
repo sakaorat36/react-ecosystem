22fc8f784b4b60447253fe3a31f36338a7b56d46
Ploy Sakaorat <sakaoratploy@gmail.com>
2023-09-08 10:48:36 +0700

    usecontext

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,80 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+// Context
+// #####A1.createContext [Provider, Consumer]
+const ThemeContext = createContext();
+/*
+#####A2.สร้าง HOC : Higher Order Component
+HOC คือ FC ที่รับ Component เข้าไปและ return Component ใหม่ออกมา
+function ThemeContextProvider(props) {
+  console.log(props);
+  return <div>{props.children}</div>;
+}
+*/
 
-function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+// Data : isDarkMode, styleObj
+// Logic : setIsDarkMode, handdleToggleTheme
+function ThemeContextProvider(props) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   const styleObj = {
     backgroundColor: isDarkMode ? "black" : "white",
     color: isDarkMode ? "white" : "black",
   };
+
   const handdleToggleTheme = () => {
     setIsDarkMode(!isDarkMode);
   };
+
+  const shareObj = { theme: styleObj, toggleTheme: handdleToggleTheme };
   return (
-    <div className="App" style={styleObj}>
+    <ThemeContext.Provider value={shareObj}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+}
+
+/*
+#####A2 Share Data and Logic ผ่าน attribute value
+- Data (State, boolean, string, object, array, etc)
+- Logic (FN ที่ใช้ handdle ต่างๆ)
+*/
+
+// #####A3. นำ Provider ไปคลอบ Children [Provider]
+// <ThemeContextProvider>
+//   <App />
+// </ThemeContextProvider>
+
+/*
+#####B1 : @Children Component ดึงค่า Shared object ผ่านตัว  useContext
+SYNTAX : useContext(ContextName)
+ex.
+const shareObj = useContext(ThemeContext)
+*/
+
+// ###################################################
+// ###################################################
+// ###################################################
+// ###################################################
+
+// UI
+function App() {
+  const s = useContext(ThemeContext);
+  console.log(s); // 42 เพราะว่า <App/> เป็น childrenของ <ThemeContextProvider> จึงสามารถดึงข้อมูลของ <ThemeContextProvider> มาได้
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  return (
+    <div className="App" style={s.theme}>
       <h1>Theme App</h1>
-      <button onClick={handdleToggleTheme}>Toggle Theme</button>
+      <button onClick={s.toggleTheme}>Toggle Theme</button>
     </div>
   );
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// 3. นำ Provider ไปคลอบ Children [Provider]
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeContextProvider>
+    <App />
+  </ThemeContextProvider>
+);
