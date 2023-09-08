a1fb1e393ba733550dd67922fa519f800ae84312
Ploy Sakaorat <sakaoratploy@gmail.com>
2023-09-08 10:00:50 +0700

    handdletoggle

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <div>Vite React</div>
-);
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  const styleObj = {
+    backgroundColor: isDarkMode ? "black" : "white",
+    color: isDarkMode ? "white" : "black",
+  };
+  const handdleToggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+  return (
+    <div className="App" style={styleObj}>
+      <h1>Theme App</h1>
+      <button onClick={handdleToggleTheme}>Toggle Theme</button>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
