fe3f79146da709c9e44f478e4e08f4ec067afafb
Ploy Sakaorat <sakaoratploy@gmail.com>
2023-09-08 14:21:40 +0700

    usenavigate

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
 import "./index.css";
 
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+
+function HomePage() {
+  const [friend, setFriend] = useState([]);
+  const navigate = useNavigate();
+
+  const fetchFriend = async () => {
+    try {
+      const response = await axios.get("/users");
+      console.log(response.data);
+      setFriend(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleNavigate = (userId) => {
+    if (userId == 1) navigate("/profile");
+    else navigate(`/profile/${userId}`);
+  };
+
+  useEffect(() => {
+    fetchFriend();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Home Page</h1>
+      {friend.map((f) => (
+        <div className="friend" key={f.id} onClick={() => handleNavigate(f.id)}>
+          <h3>{f.name}</h3>
+          <h5>
+            {f.email},Tel.{f.phone}
+          </h5>
+        </div>
+      ))}
+    </div>
+  );
+}
+function ProfilePage() {
+  return <div className="App">Profile Page</div>;
+}
+function FriendPage() {
+  return <div className="App">Friend Page</div>;
+}
+function FeedPage() {
+  return <div className="App">Feed Page</div>;
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div>Vite React</div>
+  <BrowserRouter>
+    <Link to="/">home</Link>
+    <Link to="/profile">profile</Link>
+    <Link to="/profile/5">friend</Link>
+    <Link to="/feed">feed</Link>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/profile/:id" element={<FriendPage />} />
+      <Route path="/feed" element={<FeedPage />} />
+    </Routes>
+  </BrowserRouter>
 );
+// npm i react-router-dom
