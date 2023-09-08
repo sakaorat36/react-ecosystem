517ded5298d855bef6e468d9387aa6f0b8fec1dd
Ploy Sakaorat <sakaoratploy@gmail.com>
2023-09-08 16:55:05 +0700

    mui-botton

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import Button from "@mui/material/Button";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <div>Vite React</div>
-);
+function App() {
+  return (
+    <div>
+      <Button variant="text">botton</Button>
+      <Button variant="contained">botton</Button>
+      <Button variant="contained" disabled>
+        botton
+      </Button>
+      <Button variant="outlined">botton</Button>
+
+      <Button variant="contained" color="primary">
+        botton
+      </Button>
+      <Button variant="contained" color="secondary">
+        botton
+      </Button>
+      <Button variant="contained" color="success">
+        botton
+      </Button>
+      <Button
+        variant="contained"
+        color="error"
+        onClick={() => alert("Clicked")}
+        sx={{ padding: "10px", marginTop: "20px", borderRadius: "10px" }}
+      >
+        botton
+      </Button>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
